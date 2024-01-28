6ba0d63b418ad7eaaf8ad67b9c608cc0331661aa
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-01-28 09:23:44 -0300

    refactor: remove some logs and change variable name

--- src/requests/githutRequest.ts
+++ src/requests/githutRequest.ts
@@ -3,9 +3,8 @@ import { githubUserType } from '../types/githubUserType';
 
 export const githubUserRequest = async (username: string):Promise<githubUserType> => {
   try {
-    const data = await fetch(`https://api.github.com/users/${username}`);
-    const dataJson = await data.json();
-    console.log(dataJson);
+    const userData = await fetch(`https://api.github.com/users/${username}`);
+    const dataJson = await userData.json();
     return dataJson;
   } catch(error: any) {
     throw new Error(error);
@@ -14,9 +13,8 @@ export const githubUserRequest = async (username: string):Promise<githubUserType
 
 export const githubRepoRequest = async (username: string):Promise<githubRepoType[]> => {
   try {
-    const data = await fetch(`https://api.github.com/users/${username}/repos`);
-    const dataJson = await data.json();
-    console.log(dataJson);
+    const reposData = await fetch(`https://api.github.com/users/${username}/repos`);
+    const dataJson = await reposData.json();
     return dataJson;
   } catch(error: any) {
     throw new Error(error);
